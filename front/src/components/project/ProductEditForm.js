bd0875c71905dcbb7041f4ed1208b3aa0650eddf
rnrn99 <giu0213@naver.com>
2022-03-17 12:02:46 +0900

    [Update] Project MVP 폴더 구축 및 파일 생성

---
+++ front/src/components/project/ProductEditForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductEditForm() {
+  return <div>ProductEditForm</div>;
+}
+
+export default ProductEditForm;
---
+++ front/src/components/project/ProjectAddForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProjectAddForm() {
+  return <div>ProjectAddForm</div>;
+}
+
+export default ProjectAddForm;
