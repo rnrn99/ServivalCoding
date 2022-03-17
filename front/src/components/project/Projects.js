d316cc05b47aa7a80158070ab65e7d55bfdb46bb
rnrn99 <giu0213@naver.com>
2022-03-17 14:02:46 +0900

    [Update] 클릭 여부에 따라 AddForm 렌더링

--- front/src/components/project/Projects.js
+++ front/src/components/project/Projects.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card, Row, Button, Col } from "react-bootstrap";
 import ProjectAddForm from "./ProjectAddForm";
 import Project from "./Project";
+import * as Api from "../../api";
 
 function Projects({ portfolioOwnerId, isEditable }) {
+  const [projects, setProjects] = useState([]); // 해당 유저의 프로젝트를 저장합니다.
+  const [clickAddBtn, setClickAddBtn] = useState(false); // 프로젝트 추가 버튼 클릭 상태를 저장합니다.
+
   return (
     <>
       <Card className="ml-3">
@@ -16,12 +20,17 @@ function Projects({ portfolioOwnerId, isEditable }) {
           </Row>
           <Row className="text-center">
             <Col>
-              <Button variant="primary">+</Button>
+              {isEditable && (
+                <Button
+                  variant="primary"
+                  onClick={() => setClickAddBtn((cur) => !cur)}
+                >
+                  +
+                </Button>
+              )}
             </Col>
           </Row>
-          <Row>
-            <ProjectAddForm />
-          </Row>
+          <Row>{clickAddBtn && <ProjectAddForm />}</Row>
         </Card.Body>
       </Card>
     </>
