97c344359a2bacff730b62d961520aea937fc549
jinah park <tbrk8585@naver.com>
2022-03-19 02:08:58 +0900

    [Update] delete 기능 test

--- front/src/components/award/Award.js
+++ front/src/components/award/Award.js
@@ -1,12 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {Card, Row, Col, Button} from "react-bootstrap";
 import AwardEditForm from './AwardEditForm';
+import { UserStateContext } from "../../App";
+import * as Api from "../../api";
 
 
 // 뿌려지는 수상이력 개별이 갖는 구조 컨퍼넌트 입니다. 
 function Award ({award, isEditable, setAwardLists}) {
     // 편집 버튼 클릭 시, AwardEditForm이 활성화 되도록 하는 state 입니다. 
     const [isEditing, setIsEditing] = useState(false);
+    const userState = useContext(UserStateContext);
+    const user_id = userState.user.id;
+
+    const deleteHandler = async (e) => {
+        await Api.delete("awards", award.id)
+
+        const deleteData = await Api.get('awardlist', user_id)
+        setAwardLists(deleteData.data)
+    }
+
+
 
     return (
          <>  
@@ -17,12 +30,13 @@ function Award ({award, isEditable, setAwardLists}) {
           :(   
             <Row className="align-items-center">
                 <Col>
-                    <p >{award.title}</p>
+                    <p style={{ marginBottom: "5px" }}>{award.title}</p>
                     <p className='text-muted'>{award.description}</p>
                 </Col> 
                 {isEditable&&
                     <Col xs lg="1">
                         <Button variant="outline-info" size="sm" className="mr-3" onClick={()=>setIsEditing(true)}>편집</Button>
+                        <Button variant="outline-danger" size="sm" className="mr-3" onClick={deleteHandler}>삭제</Button>  
                     </Col>
                 }
             </Row>
