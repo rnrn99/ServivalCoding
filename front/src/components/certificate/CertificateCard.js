2d7bac17514d0b0ad7a65d128e724c336bcff44c
theohong <theohong0234@gmail.com>
2022-03-19 15:51:28 +0900

    [Update] certificate #70 #72 #62 삭제 기능 구현 완료

--- front/src/components/certificate/CertificateCard.js
+++ front/src/components/certificate/CertificateCard.js
@@ -1,26 +1,48 @@
 import React from "react";
 import { Button, Row, Col } from "react-bootstrap";
 
-const CertificateCard = ({isEditable, checkEditing, title, description, date}) => {
-    //isEditing <<부모 Certificate 한테 전달해줘야함.
-    const handleClick = (e) => {
-        //수정하기 버튼이 눌린 것이므로 
-        //isEditing <<부모 Certificate 한테 전달해줘야함. true값을 전달.
-        e.preventDefault();
-        console.log("수정하기 버튼이 눌렸습니다.");
-        checkEditing(true);
+const CertificateCard = ({
+  isEditable,
+  checkEditing,
+  checkDeleting,
+  title,
+  description,
+  date,
+}) => {
+  //isEditing <<부모 Certificate 한테 전달해줘야함.
+  const handleClick = (e) => {
+    //수정하기 버튼이 눌린 것이므로
+    //isEditing <<부모 Certificate 한테 전달해줘야함. true값을 전달.
+    e.preventDefault();
+    if (e.target.name === "edit") {
+      console.log("수정하기 버튼이 눌렸습니다.");
+      checkEditing(true);
+    } else {
+      //삭제 버튼이 눌렸다면
+      checkDeleting(true);
     }
-    return (
-        <Row>
-            <Col>자격사항: {title}</Col>
-            <Col>{description}</Col>
-            <Col>취득일자 : {date}</Col>
-            <Col>
-                {isEditable && (<Button onClick={handleClick}>수정하기</Button>)}
-            </Col>
-            
-        </Row>
-    );
+  };
+  return (
+    <Row>
+      <Col>자격사항: {title}</Col>
+      <Col>{description}</Col>
+      <Col>취득일자 : {date}</Col>
+      <Col>
+        {isEditable && (
+          <Button name="edit" onClick={handleClick}>
+            수정하기
+          </Button>
+        )}
+      </Col>
+      <Col>
+        {isEditable && (
+          <Button name="delete" onClick={handleClick}>
+            삭제하기
+          </Button>
+        )}
+      </Col>
+    </Row>
+  );
 };
 
 export default CertificateCard;
--- front/src/components/certificate/Certificates.js
+++ front/src/components/certificate/Certificates.js
@@ -11,7 +11,7 @@ import CertificateAddForm from "./CertificateAddForm";
 // components/Portfolio에 전달된다.
 // 호출된 Certificates는 유저정보를 기반으로 DB에서 자격증정보를 받음
 // 개별 자격증 데이터는 Certificate 단위 로 관리됨
-// Certificates 모듈은 자격증의 Certificate의 리스트를 출력함 
+// Certificates 모듈은 자격증의 Certificate의 리스트를 출력함
 // 개별 Certificate는 CertificateCard의 형태로 표현됨
 // 자격증정보가 없을 경우, 추가할 경우 CertificateAddForm을 불러옴
 // 개별 자격증 Certificate의 수정이 일어날 때는 CertificateEditForm을 불러옴
@@ -24,25 +24,21 @@ import CertificateAddForm from "./CertificateAddForm";
 //    ㄴCertificateCard
 //    ㄴCertificateEditForm
 
-// 논리적인 구조일 뿐이며, 
+// 논리적인 구조일 뿐이며,
 // ./contents/certificate 폴더에 해당 모듈파일들 전부 담는다.
 
-
-//지금 포트폴리오를 보고 있는 유저가 작성자 인지 확인. 
-//작성자가 Certificates에 접근한 경우, 
+//지금 포트폴리오를 보고 있는 유저가 작성자 인지 확인.
+//작성자가 Certificates에 접근한 경우,
 //리스트를 뿌릴때 Certificate에 인자를 전달하여 CertificateEditFrom을 활성화 할수 있도록 한다. < 삭제도 포함된다.
-//작성자가 보고 있지 않은 경우에는 수정 폼을 비활성화 한다. 
+//작성자가 보고 있지 않은 경우에는 수정 폼을 비활성화 한다.
 
 //Certificates는 Portfolio 모듈에서 호출되는 시점 prop으로
 // portfolioOwnerId={portfolioOwner.id}
 // isEditable={portfolioOwner.id === userState.user?.id}
-// 를 전달받는다. 
+// 를 전달받는다.
 //
-//portfolioOwnerId 는 현재 그려지고 있는 사용자(작성자)의 id 
+//portfolioOwnerId 는 현재 그려지고 있는 사용자(작성자)의 id
 //isEditable 는 boolean 값으로 포트폴리오를 요청한 사용자일 경우에만 수정 가능하도록한다.
-
-
-
 
 //<h1>Certificates 모듈입니다.</h1>
 //Portfolio에서 전달 받은 portfolioOwnerId 로 서버에 데이터를 요청함.
@@ -50,91 +46,100 @@ import CertificateAddForm from "./CertificateAddForm";
 //<button>추가하기</button> << 활성화
 ////<CertificateAddForm /> <<버튼이 눌리면 활성화.
 const Certificates = ({ portfolioOwnerId, isEditable }) => {
+  //서버에서 받아온 자격증 데이터
+  const [certs, setCerts] = useState([]);
+  //isAdd는 자격증 항목을 추가하기 버튼을 눌렀을 때 활성화
+  //추가하기인 상황에서는 추가하기 버튼이 보여선 안됨
+  //때문에 isEditable && !isAdd 로 숨김
+  const [isAdd, setIsAdd] = useState(false);
 
-    //서버에서 받아온 자격증 데이터
-    const [certs, setCerts] = useState([]);
-    //isAdd는 자격증 항목을 추가하기 버튼을 눌렀을 때 활성화
-    //추가하기인 상황에서는 추가하기 버튼이 보여선 안됨 
-    //때문에 isEditable && !isAdd 로 숨김
-    const [isAdd, setIsAdd] = useState(false);
+  useEffect(() => {
+    Api.get("certificate-lists", portfolioOwnerId).then((res) =>
+      setCerts(res.data)
+    );
+  }, [portfolioOwnerId]);
 
-    useEffect(() => {
-        Api.get("certificate-lists", portfolioOwnerId)
-            .then((res) => setCerts(res.data));
-    }, [portfolioOwnerId]);
+  const setCertificateList = () => {
+    return certs.map((cert) => {
+      return (
+        <Certificate
+          key={cert.id}
+          cert={cert}
+          isEditable={isEditable}
+          checkModified={checkModified}
+          title={cert.title}
+          description={cert.description}
+          date={cert.date}
+        />
+      );
+    });
+  };
 
-
-
-    const setCertificateList = () => {
-        return certs.map((cert) => {
-            return <Certificate
-                key={cert.id}
-                cert={cert}
-                isEditable={isEditable}
-                checkModified={checkModified}
-                title={cert.title}
-                description={cert.description}
-                date={cert.date}
-            />;
-        });
-    };
-
-    //추가하기가 완료되었는지 체크를 위해 
-    //isAddComplete 스테이트를 건들수 있는 함수를 AddForm에 전달함
-    //AddForm에서는 완료 버튼이 눌렸을 때 결과 값들을 보내줌
-    //props에는 서버로 post할 자격증 정보가 담겨있음
-    const checkAddComplete = async (props) => {
-
-        console.log("Check Add Complete");
-        if (props) {
-            //데이터를 업데이트 합니다. 
-            const newData = {
-                ...props,
-                //user_id: portfolioOwnerId,
-            }
-            console.log(newData);
-            await Api.post("certificates", newData);
-            const res = await Api.get("certificate-lists", portfolioOwnerId);
-            setCerts(res.data);
-        }
-
-        //추가하기가 완료되어 AddForm은 닫아줍니다.
-        setIsAdd(false);
-    };
-
-    const checkModified = async (id, props) => {
-        //certificate 에서 수정이 이뤄진경우 데이터 처리. 
-        //id는 param으로 전달된 상태이기때문에 프랍만.
-        await Api.put(`certificates/${id}`, props);
-        const res = await Api.get("certificate-lists", portfolioOwnerId);
-        setCerts(res.data);
+  //추가하기가 완료되었는지 체크를 위해
+  //isAddComplete 스테이트를 건들수 있는 함수를 AddForm에 전달함
+  //AddForm에서는 완료 버튼이 눌렸을 때 결과 값들을 보내줌
+  //props에는 서버로 post할 자격증 정보가 담겨있음
+  const checkAddComplete = async (props) => {
+    console.log("Check Add Complete");
+    if (props) {
+      //데이터를 업데이트 합니다.
+      const newData = {
+        ...props,
+        //user_id: portfolioOwnerId,
+      };
+      console.log(newData);
+      await Api.post("certificates", newData);
+      const res = await Api.get("certificate-lists", portfolioOwnerId);
+      setCerts(res.data);
     }
 
-    const handleClick = (e) => {
-        e.preventDefault();
-        console.log("handleClick 추가하기 버튼이 눌렸습니다.")
-        setIsAdd(true);
-    };
+    //추가하기가 완료되어 AddForm은 닫아줍니다.
+    setIsAdd(false);
+  };
 
-    return (
-        <Card className="ml-3">
-            <Card.Body style={{ marginBottom: "15px" }}>
-                <Row>
-                    <Card.Title>자격증 정보</Card.Title>
-                </Row>
-                <Row>
-                    {setCertificateList()}
-                </Row>
-                <Row className="text-center">
-                    <Col>
-                        {(isEditable && !isAdd) && (<Button onClick={handleClick}>자격증 추가하기</Button>)}
-                        {isAdd && <CertificateAddForm checkAddComplete={checkAddComplete} />}
-                    </Col>
-                </Row>
-            </Card.Body>
-        </Card>
+  //변경사항(edit, delete)를 체크하는 함수 checkModified
+  //단위 항목에서 변경사항을 전달받아 서버로 보내는 역할
+  const checkModified = async (id, type, props) => {
+    //type 이 edit인 경우
+    //certificate 에서 수정이 이뤄진경우 데이터 처리.
+    //id는 param으로 전달된 상태이기때문에 프랍만.
+    if (type === "edit") {
+      await Api.put(`certificates/${id}`, props);
+    } else if (type === "delete") {
+      await Api.delete(`certificates/${id}`);
+    }
 
-    );
+    const res = await Api.get("certificate-lists", portfolioOwnerId);
+    setCerts(res.data);
+    //type 이 delete인 경우 삭제 요청
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("handleClick 추가하기 버튼이 눌렸습니다.");
+    setIsAdd(true);
+  };
+
+  return (
+    <Card className="ml-3">
+      <Card.Body style={{ marginBottom: "15px" }}>
+        <Row>
+          <Card.Title>자격증 정보</Card.Title>
+        </Row>
+        <Row>{setCertificateList()}</Row>
+        <Row className="text-center">
+          <Col>
+            {isEditable && !isAdd && (
+              <Button onClick={handleClick}>자격증 추가하기</Button>
+            )}
+            {isAdd && (
+              <CertificateAddForm checkAddComplete={checkAddComplete} />
+            )}
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Certificates;
