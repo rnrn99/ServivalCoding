5e68a95c2b572fb3ffbd7816fb2ea8b155862a97
rnrn99 <giu0213@naver.com>
2022-03-22 18:38:07 +0900

    [Update] 유저 검색 기능

--- front/src/components/user/Network.js
+++ front/src/components/user/Network.js
@@ -1,7 +1,14 @@
 import React, { useEffect, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import { Container, Row } from "react-bootstrap";
-import { Container, Grid } from "@mui/material";
+import {
+  Container,
+  Grid,
+  Input,
+  Box,
+  InputAdornment,
+  Typography,
+} from "@mui/material";
+import PersonSearchIcon from "@mui/icons-material/PersonSearch";
 
 import * as Api from "../../api";
 import UserNetworkCard from "./UserNetworkCard";
@@ -10,8 +17,10 @@ import { UserStateContext } from "../../App";
 function Network() {
   const navigate = useNavigate();
   const userState = useContext(UserStateContext);
-  // useState 훅을 통해 users 상태를 생성함.
-  const [users, setUsers] = useState([]);
+
+  const [users, setUsers] = useState([]); // 유저의 정보를 저장합니다.
+  const [searchWord, setSearchWord] = useState(""); // 검색어를 저장합니다.
+  const [noResults, setNoResults] = useState(null); // 검색 결과의 유무를 저장합니다.
 
   useEffect(() => {
     // 만약 전역 상태의 user가 null이라면, 로그인 페이지로 이동함.
@@ -23,12 +32,52 @@ function Network() {
     Api.get("users").then((res) => setUsers(res.data.data));
   }, [userState, navigate]);
 
+  useEffect(() => {
+    if (searchWord !== "") {
+      Api.get("users/search", searchWord).then((res) => {
+        const result = res.data.data;
+        if (result.length > 0) {
+          // 검색 결과가 있으면 검색 결과를 보여줍니다.
+          setNoResults(false);
+          setUsers(result);
+        } else {
+          // 검색 결과가 없을 경우 검색 결과 없음을 표시합니다.
+          setNoResults(true);
+        }
+      });
+    } else {
+      // 검색어가 없을 경우 전체 유저 정보를 표시합니다.
+      setNoResults(false);
+      Api.get("users").then((res) => setUsers(res.data.data));
+    }
+  }, [searchWord]);
+
   return (
     <Container sx={{ py: 8 }} maxWidth="md">
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "flex-end",
+          mb: 3,
+        }}
+      >
+        <Input
+          variant="outlined"
+          startAdornment={
+            <InputAdornment position="start">
+              <PersonSearchIcon />
+            </InputAdornment>
+          }
+          value={searchWord}
+          onChange={(e) => setSearchWord(e.target.value)}
+        />
+      </Box>
       <Grid container spacing={4}>
-        {users.map((user) => (
-          <UserNetworkCard key={user.id} user={user} />
-        ))}
+        {noResults ? (
+          <Typography>검색 결과 없음</Typography>
+        ) : (
+          users.map((user) => <UserNetworkCard key={user.id} user={user} />)
+        )}
       </Grid>
     </Container>
   );
