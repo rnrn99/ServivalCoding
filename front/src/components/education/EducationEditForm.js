dd54c398431cd0de0169a28394940fb2330f5fdc
rnrn99 <giu0213@naver.com>
2022-03-23 12:01:02 +0900

    [Update] EditForm에도 학교 선택지 추가

--- front/src/components/education/EducationEditForm.js
+++ front/src/components/education/EducationEditForm.js
@@ -1,4 +1,5 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useRef } from "react";
+import _ from "lodash";
 import {
   Box,
   TextField,
@@ -7,9 +8,12 @@ import {
   Radio,
   RadioGroup,
   Button,
+  Chip,
 } from "@mui/material";
+import SchoolIcon from "@mui/icons-material/School";
 import * as Api from "../../api";
 import { UserStateContext } from "../../App";
+import { getEducationApi } from "../../utils";
 
 function EducationEditForm({ education, setEducations, setClickEditBtn }) {
   const userState = useContext(UserStateContext); // 현재 로그인된 유저의 정보를 가져옵니다.
@@ -17,6 +21,7 @@ function EducationEditForm({ education, setEducations, setClickEditBtn }) {
   const [school, setSchool] = useState(education.school); // 학교 이름을 저장할 상태입니다.
   const [major, setMajor] = useState(education.major); // 전공을 저장할 상태입니다.
   const [educationStatus, setEducationStatus] = useState(education.position); // 재학/졸업 여부를 저장할 상태입니다.
+  const [schoolOpt, setSchoolOpt] = useState(null); // 학교 이름 검색에 따른 옵션들을 저장합니다.
 
   const statusArr = ["재학중", "학사졸업", "석사졸업", "박사졸업"]; // postion을 저장하는 배열입니다.
   const userId = userState.user.id; // 현재 로그인한 유저의 아이디를 저장합니다.
@@ -47,20 +52,58 @@ function EducationEditForm({ education, setEducations, setClickEditBtn }) {
     setClickEditBtn(false);
   };
 
+  const sendQuery = async (word) => {
+    // 학교 선택지를 가져오기 위해 api를 생성합니다.
+    const api = getEducationApi(word);
+
+    //  open api 주소로 GET 요청을 보냅니다.
+    const res = await api.get();
+
+    // 반환된 결과를 3개까지만 저장하고 학교 이름만 가져와 schoolOpt 상태에 저장합니다.
+    const data = res.data.dataSearch.content;
+    const opt = data.map((v) => v.schoolName);
+    setSchoolOpt(opt);
+  };
+
+  // 200ms마다 입력된 내용을 검색어로 지정합니다.
+  const delayedSearchWord = useRef(
+    _.debounce((q) => sendQuery(q), 200),
+  ).current;
+
+  const schoolChangeHandler = (e) => {
+    setSchool(e.target.value);
+    delayedSearchWord(e.target.value);
+  };
+
   return (
     <Box component="form" onSubmit={onSubmitHandler} sx={{ mt: 1 }}>
       <Stack spacing={2}>
         <TextField
           required
           label="학교 이름"
-          onChange={(e) => setSchool(e.target.value)}
-          defaultValue={school}
+          onChange={(e) => schoolChangeHandler(e)}
+          value={school}
         />
+        {schoolOpt && (
+          <Stack direction="row" spacing={1}>
+            {schoolOpt.map((name, i) => (
+              <Chip
+                key={"schoolOpt" + i}
+                icon={<SchoolIcon />}
+                label={name}
+                color="primary"
+                variant="outlined"
+                size="small"
+                onClick={(e) => setSchool(e.target.innerText)}
+              />
+            ))}
+          </Stack>
+        )}
         <TextField
           required
           label="전공"
           onChange={(e) => setMajor(e.target.value)}
-          defaultValue={major}
+          value={major}
         />
       </Stack>
       <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
--- front/src/utils.js
+++ front/src/utils.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 // Date를 YYYY-MM-DD의 문자열로 바꾸는 함수입니다.
 export const dateToString = (date) => {
   return (
@@ -8,3 +10,21 @@ export const dateToString = (date) => {
     date.getDate().toString().padStart(2, "0")
   );
 };
+
+// 학교 정보를 가져오기 위해 open api로 요청을 보낼 api를 만드는 함수입니다.
+export function getEducationApi(word) {
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const target = process.env.REACT_APP_TARGET;
+  return axios.create({
+    baseURL: "http://www.career.go.kr/cnet/openapi/getOpenApi",
+    params: {
+      apiKey,
+      svcType: "api",
+      svcCode: "SCHOOL",
+      contentType: "json",
+      gubun: target,
+      perPage: "3",
+      searchSchulNm: word,
+    },
+  });
+}
