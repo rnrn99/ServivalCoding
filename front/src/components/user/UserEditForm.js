e97ce5be2f10a75b4959a25026f489a0e13163c3
jinah park <tbrk8585@naver.com>
2022-03-24 00:38:34 +0900

    [Update] userEditForm switch 적용

--- front/src/components/user/UserEditForm.js
+++ front/src/components/user/UserEditForm.js
@@ -1,28 +1,40 @@
 import React, { useState } from "react";
 import * as Api from "../../api";
+import Avatar from '@mui/material/Avatar';
 import {
   Box,
   TextField,
   Stack,
   Button,
 } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import Switch from '@mui/material/Switch';
+// 스타일적용부분은 export 하단으로 옮겨 둠
+
 
 function UserEditForm({ user, setIsEditing, setUser }) {
+  const [form, setForm] = useState({
+    name: user.name,
+    email: user.email,
+    description: user.description
+  })
   //useState로 name 상태를 생성함.
-  const [name, setName] = useState(user.name);
+  // const [name, setName] = useState(user.name);
   //useState로 email 상태를 생성함.
-  const [email, setEmail] = useState(user.email);
+  // const [email, setEmail] = useState(user.email);
   //useState로 description 상태를 생성함.
-  const [description, setDescription] = useState(user.description);
-
+  // const [description, setDescription] = useState(user.description);
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     // "users/유저id" 엔드포인트로 PUT 요청함.
     const res = await Api.put("users", {
-      name,
-      email,
-      description,
+      name: form.name,
+      email: form.email,
+      description: form.description
     });
     // 유저 정보는 response의 data임.
     const updatedUser = res.data.data;
@@ -34,27 +46,46 @@ function UserEditForm({ user, setIsEditing, setUser }) {
   };
 
   return (
-    <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
-      <Stack spacing={2}>
-        <TextField
-          required
-          label="이름"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
-        <TextField
-          required
-          type="email"
-          label="이메일"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-        <TextField
-          required
-          label="정보, 인사말"
-          onChange={(e) => setDescription(e.target.value)}
-          value={description}
-        />
+    <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1, width: '400px' }}>
+      <Stack spacing={2} align="center" >
+        <label htmlFor="icon-button-file">
+        <Input accept="image/*" id="icon-button-file" type="file" />
+          <IconButton color="primary" aria-label="upload picture" component="span" >
+            <Badge color="primary" overlap="circular" badgeContent="+">
+              {avatar}
+            </Badge>
+        </IconButton>
+        </label>
+        <Stack style={{display: "inline"}} >
+          <TextField
+            required
+            label="이름"
+            onChange={(e) => setForm({...form, name: e.target.value})}
+            value={form.name}
+            sx={{ width: '320px'}}
+          />
+          <Switch {...label} defaultChecked onChange={(e)=>console.log(e.target.checked)}/>
+        </Stack>
+        <Stack style={{display: "inline"}}>
+          <TextField
+            readOnly
+            type="email"
+            label="이메일"
+            value={form.email}
+            sx={{ width: '320px'}}
+          />
+          <Switch {...label} defaultChecked onChange={(e)=>console.log(e.target.checked)}/>
+        </Stack>
+        <Stack style={{display: "inline"}}>
+          <TextField
+            required
+            label="정보, 인사말"
+            onChange={(e) => setForm({...form, description: e.target.value})}
+            value={form.description}
+            sx={{ width: '320px'}}
+          />
+          <Switch {...label} defaultChecked onChange={(e)=>console.log(e.target.checked)}/>
+        </Stack>
       </Stack>  
       <Stack
         direction="row"
@@ -77,3 +108,20 @@ function UserEditForm({ user, setIsEditing, setUser }) {
 }
 
 export default UserEditForm;
+
+const Input = styled('input')({
+  display: 'none',
+});
+
+const shapeStyles = { width: 150, height: 150 };
+const shapeCircleStyles = { borderRadius: '50%' };
+const avatar = (
+  <Avatar
+    component="span"
+    alt="Remy Sharp"
+    src='http://placekitten.com/200/200'
+    sx={{ ...shapeStyles, ...shapeCircleStyles }}
+  />
+);
+
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
