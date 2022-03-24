16be721e1ec4c2b71d1b187f6e039d35ab77cc0c
jinah park <tbrk8585@naver.com>
2022-03-24 16:46:56 +0900

    [update] 유저권한기능 미완성

--- front/src/components/user/UserCard.js
+++ front/src/components/user/UserCard.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 import React, {useState, useEffect} from "react";
-=======
-import React, { useState, useEffect } from "react";
->>>>>>> dev
 import * as Api from "../../api";
 
 //mui
 import { Button, Container } from "@mui/material";
-<<<<<<< HEAD
 import Avatar from '@mui/material/Avatar';
 import {
   Card,
@@ -29,52 +24,18 @@ function UserCard({ user, setUser, setIsEditing, isEditable, portfolioOwnerId })
   // 포트폴리오 주인이 바뀔때 마다 갱신
   useEffect(()=>{
     Api.get("users", portfolioOwnerId).then((res)=> {
-      console.log(res.data.data)
-      setHeartCount(res.data.data.like.count)
-      setClickHeart(res.data.data.isLikedByThisUser)
+      setHeartCount(res.data.user.like.count)
+      setClickHeart(res.data.user.isLikedByThisUser)
     })
   }, [portfolioOwnerId]); 
-=======
-import Avatar from "@mui/material/Avatar";
-import { Card, CardContent, Typography } from "@mui/material";
-import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
-import Favorite from "@mui/icons-material/Favorite";
-import EditIcon from "@mui/icons-material/Edit";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import WysiwygIcon from "@mui/icons-material/Wysiwyg"; // 정녕 블로그 아이콘이 없답니까../
-// import Link from '@material-ui/core/Link'; // 추가 설치 드 가야합니다.. > 아이콘에 링크 두려면(깃헙, 블로그, 인스타 등)
-
-function UserCard({
-  user,
-  setUser,
-  setIsEditing,
-  isEditable,
-  portfolioOwnerId,
-}) {
-  const [clickHeart, setClickHeart] = useState(false);
-  const [heartCount, setHeartCount] = useState(0);
-
-  // 포트폴리오 주인이 바뀔때 마다 갱신
-  useEffect(() => {
-    Api.get("users", portfolioOwnerId).then((res) => {
-      setHeartCount(res.data.user.like.count);
-      setClickHeart(res.data.user.isLikedByThisUser);
-    });
-  }, [portfolioOwnerId]); // 저에게 경고를 하는 것 같은데, setUser 너무 자주 바뀌니깐 useCallback 처리하라는 말일까요?
->>>>>>> dev
 
   const onHeartHandler = async () => {
     await Api.post(`users/${user.id}/likes`);
 
-<<<<<<< HEAD
-      await Api.post(`users/${user.id}/likes`);
-
       const res = await Api.get("users", portfolioOwnerId);
-        console.log(res.data.data)
-        setUser(res.data.data)
-        setHeartCount(res.data.data.like.count)
-        setClickHeart(res.data.data.isLikedByThisUser)
+        setUser(res.data.user)
+        setHeartCount(res.data.user.like.count)
+        setClickHeart(res.data.user.isLikedByThisUser)
   }
 
   return (
@@ -117,76 +78,6 @@ function UserCard({
     </CardContent> 
   </Card>  
   </> 
-=======
-    const res = await Api.get("users", portfolioOwnerId);
-    setUser(res.data.user);
-    setHeartCount(res.data.user.like.count);
-    setClickHeart(res.data.user.isLikedByThisUser);
-  };
-
-  return (
-    <>
-      <Card sx={{ marginBottom: "20px" }}>
-        <CardContent align="center">
-          <Container style={{ marginBottom: "25px" }}>
-            <Avatar
-              alt="Remy Sharp"
-              src="http://placekitten.com/200/200"
-              sx={{
-                width: 180,
-                height: 180,
-                marginTop: "15px",
-                marginBottom: "15px",
-                border: "3px double #9999A1",
-              }}
-            />
-            <Typography style={{ fontWeight: "bold", fontSize: "22px" }}>
-              {user?.name}
-            </Typography>
-            <Typography
-              className="text-muted"
-              style={{ fontSize: "13px", marginBottom: "12px" }}
-            >
-              ({user?.email})
-            </Typography>
-            <Typography>"{user?.description}"</Typography>
-          </Container>
-          <Container className="text-muted" style={{ fontSize: "12px" }}>
-            <Button
-              startIcon={clickHeart ? <Favorite /> : <FavoriteBorder />}
-              onClick={onHeartHandler}
-              style={
-                clickHeart
-                  ? { color: "red", minWidth: "0", paddingRight: "0" }
-                  : { color: "grey", minWidth: "0", paddingRight: "0" }
-              }
-            />
-            <Typography
-              style={{ display: "inline", fontSize: "13px", margin: "0" }}
-            >
-              {heartCount}명이 좋아합니다
-            </Typography>
-          </Container>
-          <Container sx={{ marginBottom: "10px" }}>
-            <GitHubIcon sx={{ marginRight: "4px" }} />
-            <InstagramIcon sx={{ marginRight: "3px" }} />
-            <WysiwygIcon />
-          </Container>
-          <Container sx={{ paddingBottom: "0" }}>
-            {isEditable && (
-              <Button
-                onClick={() => setIsEditing(true)}
-                startIcon={<EditIcon />}
-              >
-                {" "}
-                편집
-              </Button>
-            )}
-          </Container>
-        </CardContent>
-      </Card>
-    </>
->>>>>>> dev
   );
 }
 
--- front/src/components/user/UserEditForm.js
+++ front/src/components/user/UserEditForm.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as Api from "../../api";
-<<<<<<< HEAD
 import Avatar from '@mui/material/Avatar';
 import {
   Box,
@@ -14,16 +13,19 @@ import Badge from '@mui/material/Badge';
 import Switch from '@mui/material/Switch';
 // 스타일적용부분은 export 하단으로 옮겨 둠
 
-=======
-import { Box, TextField, Stack, Button } from "@mui/material";
->>>>>>> dev
 
 function UserEditForm({ user, setIsEditing, setUser }) {
   const [form, setForm] = useState({
-    name: user.name,
-    email: user.email,
-    description: user.description
+    name: "",
+    email: "",
+    description: "",
   })
+  const [emailPermission, setEmailPermission] = useState(null)
+  const [descPermission, setDescPermission] = useState(null)
+
+  // userstate > user/current
+  // 확인 > permission 정보를 > setUser 업데이트 
+  // userCard 
 
   //useState로 name 상태를 생성함.
   // const [name, setName] = useState(user.name);
@@ -31,6 +33,19 @@ function UserEditForm({ user, setIsEditing, setUser }) {
   // const [email, setEmail] = useState(user.email);
   //useState로 description 상태를 생성함.
   // const [description, setDescription] = useState(user.description);
+
+  useEffect(() => {
+    Api.get("users/current").then((res)=>{
+      const result = res.data.user
+      setForm((cur)=>{
+        const newForm = {...cur, email: result.email, description: result.description, name: result.name}
+        return newForm
+      })
+      setEmailPermission(result.permission.email)
+      setDescPermission(result.permission.description)
+    })
+  },[])
+
   
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -39,20 +54,30 @@ function UserEditForm({ user, setIsEditing, setUser }) {
     const res = await Api.put("users", {
       name: form.name,
       email: form.email,
-      description: form.description
+      description: form.description,
+      permission: {email: emailPermission, description: descPermission}
     });
     // 유저 정보는 response의 data임.
     const updatedUser = res.data.user;
+
     // 해당 유저 정보로 user을 세팅함.
   
-    setUser(updatedUser);
+    setUser((cur)=>{
+      const result = {...cur, 
+        name: updatedUser.name,
+        email: updatedUser.email,
+        description: updatedUser.description,
+        permission: {email: updatedUser.permission.email, description: updatedUser.permission.description}
+      }
+      console.log(result);
+      return result;  
+    });
 
     // isEditing을 false로 세팅함.
     setIsEditing(false);
   };
 
   return (
-<<<<<<< HEAD
     <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1, width: '400px' }}>
       <Stack spacing={2} align="center" >
         <label htmlFor="icon-button-file">
@@ -75,13 +100,13 @@ function UserEditForm({ user, setIsEditing, setUser }) {
         </Stack>
         <Stack style={{display: "inline"}}>
           <TextField
-            readOnly
+            disabled
             type="email"
             label="이메일"
             value={form.email}
             sx={{ width: '320px'}}
           />
-          <Switch {...label} defaultChecked onChange={(e)=>console.log(e.target.checked)}/>
+           <Switch {...label} checked={emailPermission} onChange={(e)=>setEmailPermission(e.target.checked)}/> 
         </Stack>
         <Stack style={{display: "inline"}}>
           <TextField
@@ -91,33 +116,9 @@ function UserEditForm({ user, setIsEditing, setUser }) {
             value={form.description}
             sx={{ width: '320px'}}
           />
-          <Switch {...label} defaultChecked onChange={(e)=>console.log(e.target.checked)}/>
+          <Switch {...label} checked={descPermission} onChange={(e)=>setDescPermission(e.target.checked)}/>
         </Stack>
       </Stack>  
-=======
-    <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
-      <Stack spacing={2}>
-        <TextField
-          required
-          label="이름"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
-        <TextField
-          required
-          type="email"
-          label="이메일"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-        <TextField
-          required
-          label="정보, 인사말"
-          onChange={(e) => setDescription(e.target.value)}
-          value={description}
-        />
-      </Stack>
->>>>>>> dev
       <Stack
         direction="row"
         spacing={2}
@@ -137,6 +138,7 @@ function UserEditForm({ user, setIsEditing, setUser }) {
     </Box>
   );
 }
+
 
 export default UserEditForm;
 
