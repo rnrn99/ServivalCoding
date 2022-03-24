de8f1820190008c544ff66b2c23cd7e006d896c9
rnrn99 <giu0213@naver.com>
2022-03-24 17:38:36 +0900

    [Update] visible 분기처리

--- front/src/components/user/UserEditForm.js
+++ front/src/components/user/UserEditForm.js
@@ -1,31 +1,25 @@
 import React, { useState, useEffect } from "react";
 import * as Api from "../../api";
-import Avatar from '@mui/material/Avatar';
-import {
-  Box,
-  TextField,
-  Stack,
-  Button,
-} from "@mui/material";
-import { styled } from '@mui/material/styles';
-import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
-import Switch from '@mui/material/Switch';
+import Avatar from "@mui/material/Avatar";
+import { Box, TextField, Stack, Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+import Switch from "@mui/material/Switch";
 // 스타일적용부분은 export 하단으로 옮겨 둠
-
 
 function UserEditForm({ user, setIsEditing, setUser }) {
   const [form, setForm] = useState({
     name: "",
     email: "",
     description: "",
-  })
-  const [emailPermission, setEmailPermission] = useState(null)
-  const [descPermission, setDescPermission] = useState(null)
+  });
+  const [emailPermission, setEmailPermission] = useState(null);
+  const [descPermission, setDescPermission] = useState(null);
 
   // userstate > user/current
-  // 확인 > permission 정보를 > setUser 업데이트 
-  // userCard 
+  // 확인 > permission 정보를 > setUser 업데이트
+  // userCard
 
   //useState로 name 상태를 생성함.
   // const [name, setName] = useState(user.name);
@@ -35,18 +29,22 @@ function UserEditForm({ user, setIsEditing, setUser }) {
   // const [description, setDescription] = useState(user.description);
 
   useEffect(() => {
-    Api.get("users/current").then((res)=>{
-      const result = res.data.user
-      setForm((cur)=>{
-        const newForm = {...cur, email: result.email, description: result.description, name: result.name}
-        return newForm
-      })
-      setEmailPermission(result.permission.email)
-      setDescPermission(result.permission.description)
-    })
-  },[])
+    Api.get("users/current").then((res) => {
+      const result = res.data.user;
+      setForm((cur) => {
+        const newForm = {
+          ...cur,
+          email: result.email,
+          description: result.description,
+          name: result.name,
+        };
+        return newForm;
+      });
+      setEmailPermission(result.permission.email);
+      setDescPermission(result.permission.description);
+    });
+  }, []);
 
-  
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -55,70 +53,84 @@ function UserEditForm({ user, setIsEditing, setUser }) {
       name: form.name,
       email: form.email,
       description: form.description,
-      permission: {email: emailPermission, description: descPermission}
+      permission: { email: emailPermission, description: descPermission },
     });
     // 유저 정보는 response의 data임.
     const updatedUser = res.data.user;
 
     // 해당 유저 정보로 user을 세팅함.
-  
-    setUser((cur)=>{
-      const result = {...cur, 
-        name: updatedUser.name,
-        email: updatedUser.email,
-        description: updatedUser.description,
-        permission: {email: updatedUser.permission.email, description: updatedUser.permission.description}
-      }
-      console.log(result);
-      return result;  
-    });
+    setUser(updatedUser);
 
     // isEditing을 false로 세팅함.
     setIsEditing(false);
   };
 
   return (
-    <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1, width: '400px' }}>
-      <Stack spacing={2} align="center" >
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{ mt: 1, width: "400px" }}
+    >
+      <Stack spacing={2} align="center">
         <label htmlFor="icon-button-file">
-        <Input accept="image/*" id="icon-button-file" type="file" />
-          <IconButton color="primary" aria-label="upload picture" component="span" >
-            <Badge  overlap="circular" badgeContent="+" style={{ bgColor: '#C7A27C'}}>
+          <Input accept="image/*" id="icon-button-file" type="file" />
+          <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="span"
+          >
+            <Badge
+              overlap="circular"
+              badgeContent="+"
+              style={{ bgColor: "#C7A27C" }}
+            >
               {avatar}
             </Badge>
-        </IconButton>
+          </IconButton>
         </label>
-        <Stack style={{display: "inline"}} >
+        <Stack style={{ display: "inline" }}>
           <TextField
             required
             label="이름"
-            onChange={(e) => setForm({...form, name: e.target.value})}
+            onChange={(e) => setForm({ ...form, name: e.target.value })}
             value={form.name}
-            sx={{ width: '320px'}}
+            sx={{ width: "320px" }}
           />
-          <Switch {...label} defaultChecked onChange={(e)=>console.log(e.target.checked)}/>
+          <Switch
+            {...label}
+            defaultChecked
+            onChange={(e) => console.log(e.target.checked)}
+          />
         </Stack>
-        <Stack style={{display: "inline"}}>
+        <Stack style={{ display: "inline" }}>
           <TextField
             disabled
             type="email"
             label="이메일"
             value={form.email}
-            sx={{ width: '320px'}}
+            sx={{ width: "320px" }}
           />
-           <Switch {...label} checked={emailPermission} onChange={(e)=>setEmailPermission(e.target.checked)}/> 
+          <Switch
+            {...label}
+            checked={emailPermission}
+            onChange={(e) => setEmailPermission(e.target.checked)}
+          />
         </Stack>
-        <Stack style={{display: "inline"}}>
+        <Stack style={{ display: "inline" }}>
           <TextField
             required
             label="정보, 인사말"
-            onChange={(e) => setForm({...form, description: e.target.value})}
+            onChange={(e) => setForm({ ...form, description: e.target.value })}
             value={form.description}
-            sx={{ width: '320px'}}
+            sx={{ width: "320px" }}
           />
-          <Switch {...label} checked={descPermission} onChange={(e)=>setDescPermission(e.target.checked)}/>
+          <Switch
+            {...label}
+            checked={descPermission}
+            onChange={(e) => setDescPermission(e.target.checked)}
+          />
         </Stack>
-      </Stack>  
+      </Stack>
       <Stack
         direction="row"
         spacing={2}
@@ -139,22 +151,21 @@ function UserEditForm({ user, setIsEditing, setUser }) {
   );
 }
 
-
 export default UserEditForm;
 
-const Input = styled('input')({
-  display: 'none',
+const Input = styled("input")({
+  display: "none",
 });
 
 const shapeStyles = { width: 150, height: 150 };
-const shapeCircleStyles = { borderRadius: '50%' };
+const shapeCircleStyles = { borderRadius: "50%" };
 const avatar = (
   <Avatar
     component="span"
     alt="Remy Sharp"
-    src='http://placekitten.com/200/200'
+    src="http://placekitten.com/200/200"
     sx={{ ...shapeStyles, ...shapeCircleStyles }}
   />
 );
 
-const label = { inputProps: { 'aria-label': 'Switch demo' } };
+const label = { inputProps: { "aria-label": "Switch demo" } };
